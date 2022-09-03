a38b7d2325827428b7f0887d455b10e695df2e69
kbaba1001 <kbaba1001@users.noreply.github.com>
2022-09-03 10:00:45 +0900

    change directory structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# food-kazuki-hp
+
+## 素材など
+
+- 罫線
+  - http://free-line-design.com
---
+++ _config.js
@@ -0,0 +1,21 @@
+import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import base_path from "lume/plugins/base_path.ts";
+import terser from "lume/plugins/terser.ts";
+import date from "lume/plugins/date.ts";
+import attributes from "lume/plugins/attributes.ts";
+import parcelCss from "lume/plugins/parcel_css.ts";
+
+const site = lume();
+
+site.ignore("README.md", "CHANGELOG.md", "node_modules");
+site.use(attributes());
+site.use(base_path());
+site.use(date());
+site.use(postcss());
+site.use(terser());
+site.use(parcelCss());
+site.copy("img");
+site.copy("site.webmanifest");
+
+export default site;
